a232b43fe6ebc5c131431695da28bfc07f9b68b2
Bulemero <bulemero@gmail.com>
2019-05-13 16:40:36 +0200

    created cities filter and completed sprint 2

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -5,7 +5,9 @@ class allCities extends React.Component {
     super(props);
     this.state = {
       items: [],
-      isLoaded: false
+      isLoaded: false,
+
+      cityFilter: ""
     };
   }
 
@@ -23,16 +25,37 @@ class allCities extends React.Component {
       });
   }
 
+  handleChange = e => {
+    this.setState({
+      cityFilter: e.target.value
+    });
+  };
+
   render() {
     var { isLoaded, items } = this.state;
-
+    console.log(this.state.items);
+    //let filteredCities = this.state.cities;
+    let filteredCities = this.state.items.filter(item => {
+      let cityName = item.city.toLowerCase();
+      console.log(cityName);
+      return cityName.match(this.state.cityFilter.toLowerCase());
+    });
     if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
         <div className="App">
+          <div>
+            <label htmlFor="filter">Filter by City: </label>
+            <input
+              type="text"
+              id="filter"
+              value={this.state.cityFilter}
+              onChange={this.handleChange}
+            />
+          </div>
           <ul>
-            {items.map(item => (
+            {filteredCities.map(item => (
               <li key={item._id}>
                 {item.city} | {item.country}
               </li>
